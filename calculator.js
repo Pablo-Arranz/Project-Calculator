d73f050f121e6a756eba446908813d0294168f26
Pablo-Arranz <pabloarranz9@gmail.com>
2022-10-02 11:57:34 -0500

    Create function to store display value

--- calculator.js
+++ calculator.js
@@ -27,6 +27,8 @@ function operate(operator, a, b) {
 }
 
 const display = document.getElementById('display');
+const numDisplay = document.getElementById('numDisplay');
+const opDisplay = document.getElementById('opDisplay');
 const btn0 = document.getElementById('0');
 const btn1 = document.getElementById('1');
 const btn2 = document.getElementById('2');
@@ -42,12 +44,34 @@ const btnSub = document.getElementById('-');
 const btnMult = document.getElementById('*');
 const btnDiv = document.getElementById('/');
 
+let displayValue = 0;
+
 function showDisplay() {
-    const buttons = document.querySelectorAll('button');
-    buttons.forEach(e => {
+    const numbers = document.querySelectorAll('.number');
+    numbers.forEach(e => {
         e.addEventListener('click', function(){
-            display.textContent = e.textContent;
+            numDisplay.textContent += e.textContent;
+            displayValue = display.textContent;   
+            createOp();
+        })        
+    }) 
+}
+showDisplay();
+
+let operator;
+
+function createOp() {
+    const operators = document.querySelectorAll('.operator');
+    operators.forEach(e => {
+        e.addEventListener('click', function(){
+            operator = e.textContent;
+            opDisplay.textContent = operator;
         })
     })
 }
-showDisplay();
+
+function calculate() {
+    let a = displayValue;
+    numDisplay.textContent = '';
+    opDisplay.textContent = '';
+}
